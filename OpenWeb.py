6502d47ef28faeebbe4adc9a3e80e4e5a765a903
Vinil Mehta <vinci141@gmail.com>
2019-11-14 11:25:48 +0530

    Update OpenWeb.py

--- OpenWeb.py
+++ OpenWeb.py
@@ -10,17 +10,25 @@ pyperclip
 """
 import webbrowser as wb
 import sys
+from typing import List
+
 import pyperclip
 
+git = "https://github.com/Vinci141"
+gmap = "https://www.google.com/maps/place/"
+
+# input_data: List[str] = [git, gmap]
 NoOfArg = len(sys.argv)
 if NoOfArg > 1:
     # get address from argv
     address = ' '.join(sys.argv[1:])
     print('{} arguments provided'.format(NoOfArg))
+    print(address)
 else:
     # get address from clipboard
     address = pyperclip.paste()
 
-# un comment below line to find a place in Google maps
-# wb.open('https://www.google.com/maps/place/' + address)
-wb.open("https://github.com/Vinci141")
+if 'map' in address:
+    wb.open(f"{gmap}{address[3:]}")
+elif address == 'git':
+    wb.open("https://github.com/Vinci141")
